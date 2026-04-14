c8300bdaa42bca173cb976d29ca39809f50aa287
ksyq12 <ksyq12@daum.net>
2026-04-14 22:17:55 +0900

    chore: initial project bootstrap

---
+++ jest.config.ts
@@ -0,0 +1,13 @@
+import type { Config } from 'jest';
+
+const config: Config = {
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+  roots: ['<rootDir>/src', '<rootDir>/test'],
+  testMatch: ['**/*.test.ts'],
+  collectCoverageFrom: ['src/**/*.ts', '!src/**/*.d.ts', '!src/index.ts'],
+  coverageDirectory: 'coverage',
+  setupFilesAfterEnv: [],
+};
+
+export default config;
